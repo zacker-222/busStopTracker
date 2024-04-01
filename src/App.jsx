78600e262101e73f84fc5c22fb1a6a5aa639018f
zacker-222 <64682140+zacker-222@users.noreply.github.com>
2024-04-01 15:40:42 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BottomNavigation from './BottomNavigation';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='block'>Welcome to BusStop Tracker</h1>
+      <BottomNavigation />
+    </div>
+  )
+}
+
+export default App;
